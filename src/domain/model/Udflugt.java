df3187425dd8003be4bf7b756690f220cfd35fe2
Martin Slyngborg <slyngborgmartin@gmail.com>
2024-11-21 09:42:44 +0100

    adresse, konference, udflugt

--- src/domain/model/Udflugt.java
+++ src/domain/model/Udflugt.java
@@ -1,4 +1,24 @@
 package domain.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Udflugt {
+    private String navn;
+    private Addresse addresse;
+    private LocalDate dato;
+    private LocalTime klokkeslætFra;
+    private LocalTime klokkeslætTil;
+    private String beskrivelse;
+    private double pris;
+
+    public Udflugt(String navn, Addresse addresse, LocalDate dato, LocalTime klokkeslætFra, LocalTime klokkeslætTil, String beskrivelse, double pris) {
+        this.navn = navn;
+        this.addresse = addresse;
+        this.dato = dato;
+        this.klokkeslætFra = klokkeslætFra;
+        this.klokkeslætTil = klokkeslætTil;
+        this.beskrivelse = beskrivelse;
+        this.pris = pris;
+    }
 }
