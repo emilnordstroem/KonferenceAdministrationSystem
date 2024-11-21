c5ee7522390f9a0b98a061f9512103bf336ba007
Martin Slyngborg <slyngborgmartin@gmail.com>
2024-11-21 10:07:22 +0100

    adresse, konference, udflugt

--- src/domain/model/Konference.java
+++ src/domain/model/Konference.java
@@ -18,4 +18,60 @@ public class Konference {
         this.slutDato = slutDato;
         this.prisPrDag = prisPrDag;
     }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public LocalDate getStartDato() {
+        return startDato;
+    }
+
+    public void setStartDato(LocalDate startDato) {
+        this.startDato = startDato;
+    }
+
+    public LocalDate getSlutDato() {
+        return slutDato;
+    }
+
+    public void setSlutDato(LocalDate slutDato) {
+        this.slutDato = slutDato;
+    }
+
+    public ArrayList<Udflugt> getUdflugter() {
+        return new ArrayList<>(udflugter);
+    }
+
+    public void setUdflugter(ArrayList<Udflugt> udflugter) {
+        this.udflugter = udflugter;
+    }
+
+    public ArrayList<Hotel> getHoteller() {
+        return hoteller;
+    }
+
+    public void setHoteller(ArrayList<Hotel> hoteller) {
+        this.hoteller = hoteller;
+    }
+
+    public ArrayList<Tilmelding> getTilmeldinger() {
+        return tilmeldinger;
+    }
+
+    public void setTilmeldinger(ArrayList<Tilmelding> tilmeldinger) {
+        this.tilmeldinger = tilmeldinger;
+    }
+
+    public double getPrisPrDag() {
+        return prisPrDag;
+    }
+
+    public void setPrisPrDag(double prisPrDag) {
+        this.prisPrDag = prisPrDag;
+    }
 }
