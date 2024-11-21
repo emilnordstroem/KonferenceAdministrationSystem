5bdcd22f0e0cc134b969f95b0e9ce12b07cceec9
emilnordstroem <emilstoeve@outlook.com>
2024-11-21 09:44:53 +0100

    Deltager.java and Firma.java

--- src/domain/model/Deltager.java
+++ src/domain/model/Deltager.java
@@ -1,4 +1,32 @@
 package domain.model;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Deltager {
+    private int id;
+    private String fuldeNavn;
+    private String telefonnummer;
+    private Addresse addresse;
+    private ArrayList<Tilmelding> tilmeldingsList = new ArrayList<>();
+    private Firma firma;
+
+    public Deltager(String fuldeNavn, String telefonnummer, Addresse addresse,  Firma firma){
+        this.id = generateID();
+        this.fuldeNavn = fuldeNavn;
+        this.telefonnummer = telefonnummer;
+        this.addresse = addresse;
+        this.firma = firma;
+    }
+
+    public void setTilmeldingsList (Tilmelding tilmelding){
+        if(!tilmeldingsList.contains(tilmelding)){
+            tilmeldingsList.add(tilmelding);
+            System.out.println("Tilmelding tilføjet til deltagers tilmeldingslist");
+        }
+    }
+
+    private int generateID(){
+        return new Random().nextInt(111_111, 999_999);
+    }
 }
--- src/domain/model/Firma.java
+++ src/domain/model/Firma.java
@@ -1,4 +1,11 @@
 package domain.model;
 
 public class Firma {
+    private String name;
+    private String telefonnummer;
+
+    public Firma(String name, String telefonnummer) {
+        this.name = name;
+        this.telefonnummer = telefonnummer;
+    }
 }
