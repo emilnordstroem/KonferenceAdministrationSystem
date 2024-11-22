cedc8e946d266e57b631350d9bcf7d0c90e4aa1c
Martin Slyngborg <slyngborgmartin@gmail.com>
2024-11-22 09:15:32 +0100

    UC1 og UC2

--- src/domain/controller/Controller.java
+++ src/domain/controller/Controller.java
@@ -1,5 +1,25 @@
 package domain.controller;
 
+import domain.model.Addresse;
+import domain.model.Deltager;
+import domain.model.Konference;
+import storage.Storage;
+
+import java.time.LocalDate;
+
 public class Controller {
+    // K1, K2, UC1
+    public static Deltager opretDeltager(String navn, String telefonNummer, Addresse addresse) {
+        Deltager deltager = new Deltager(navn, telefonNummer, addresse);
+        Storage.addDeltager(deltager);
+        return deltager;
+    }
+
+    // K3, UC2
+    public static Konference opretKonference(String navn, LocalDate startDato, LocalDate slutDato, double afgiftPerDag) {
+        Konference konference = new Konference(navn, startDato, slutDato, afgiftPerDag);
+        Storage.addKonference(konference);
+        return konference;
+    }
 
 }
